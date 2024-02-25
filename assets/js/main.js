23845e1780165b58e11f2cb23195e86c0d6f5366
Camila <yourname@example.com>
2024-02-25 21:04:14 +0000

    feat: Add responsive menu bar css and JS functionalit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// responsive navbar menu
+const navEl = document.querySelector('.nav');
+const hamburgerEl = document.querySelector('.hamburger');
+
+
+hamburgerEl.addEventListener('click', () => {
+    navEl.classList.toggle('nav--open'); 
+    hamburgerEl.classList.toggle('hamburger--open');
+});
+
+navEl.addEventListener('click', () => {
+    navEl.classList.remove('nav--open'); 
+    hamburgerEl.classList.remove('hamburger--open');
+});
