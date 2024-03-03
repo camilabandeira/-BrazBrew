d709214fa1a1358175f7b70eb97e72d4c1557dbf
Camila <yourname@example.com>
2024-03-03 12:12:12 +0000

    feat: add thank-you msg post-contact submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,13 @@ navEl.addEventListener('click', () => {
     navEl.classList.remove('nav--open'); 
     hamburgerEl.classList.remove('hamburger--open');
 });
+
+// form thank you message
+
+document.getElementById('contact-us-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    this.style.display = 'none';
+
+    document.getElementById('thank-you-msg').style.display = 'block';
+});
