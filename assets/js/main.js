863dc623a516baef0d104f705e35b31082ad5da0
Camila <yourname@example.com>
2024-03-03 13:59:31 +0000

    feat: add alert for newsletter signup

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,26 @@ document.getElementById('contact-us-form').addEventListener('submit', function(e
 
     document.getElementById('thank-you-msg').style.display = 'block';
 });
+
+// newsletter-form
+
+document.getElementById('newsletter-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents the traditional form submission.
+    
+    const emailInput = document.querySelector('[name="email"]');
+    const email = emailInput.value;
+
+    showCustomAlert();
+    
+    console.log('Email for subscription:', email);
+
+    emailInput.value = '';
+});
+
+function showCustomAlert() {
+    document.getElementById("customAlert").style.display = "block";
+}
+
+function closeCustomAlert() {
+    document.getElementById("customAlert").style.display = "none";
+}
